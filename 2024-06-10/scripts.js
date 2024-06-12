c4b025035e9862e507dfc94670b4cc2c08a1e5f1
CeliaGallegoGomez <cela17@hotmail.es>
2024-06-12 18:51:30 +0200

    update

---
+++ 2024-06-10/scripts.js
@@ -0,0 +1,82 @@
+// for (let index =0;index<Array.length;index++){
+//     const element
+// }
+
+// let numeros = [1,6,9,2,3,1,10,4,5,7,8,2]
+
+// function imprimirNumerosAltos(coleccion) {
+//     for(let i =0;i<coleccion.length;i++){
+//         console.log(i)}
+// }
+// imprimirNumerosAltos(numeros)
+
+// let numeros = [1,6,9,2,3,1,10,4,5,7,8,2]
+
+// function imprimirNumerosAltos(coleccion) {
+//     for(let i =0;i<coleccion.length;i++){
+//         console.log(i)
+//         console.log(coleccion[i])}
+// }
+// imprimirNumerosAltos(coleccion)
+
+// let numeros = [1,6,9,2,3,1,10,4,5,7,8,2]
+
+// for(let index=0:index<numeros.length;index++){
+//     let numero=numeros[index];
+
+//     if (numero >5){
+//         console.log(`Indice: ${index}, numero ${numero}`);
+//     }}
+
+//     function imprimirNumerosAltos(coleccion){
+//     for(let index=0:index<numeros.length;index++){
+//     let numero=numeros [index];
+//     console.log(`Indice: ${index}, numero ${numero}`);
+
+//     if (numero >5){
+//         console.log(numero);
+//     }
+// }
+// }
+// imprimirNumerosAltos(coleccion)
+
+let numeros = [1, 6, 9, 2, 3, 1, 10, 4, 5, 7, 8, 2];
+
+for (let index = 0; index < numeros.length; index++) {
+  let numero = numeros[index];
+
+  if (numero > 5) {
+    console.log(`Indice: ${index}, número mayor que 5: ${numero}`);
+  }
+}
+
+//imprimir los numeros del 1 al 10 que sean mayores de 5
+
+for (let index = 1; index <= 10; index++) {
+  if (index > 5) {
+    console.log(index);
+  }
+}
+
+for (let index = 6; index <= 10; index++) {
+  console.log(index);
+}
+
+//imprimir los numeros del 1 al 10 que sean menores de 5
+
+for (let index = 1; index <= 10; index++) {
+  if (index < 5) {
+    console.log(index);
+  }
+}
+
+//edad adulta
+
+let edad = 36;
+let condicion = edad >= 18;
+if (condicion) {
+  console.log("puede beber alcohol");
+} else {
+  console.log("no puede beber alcohol");
+}
+
